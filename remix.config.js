7fc5929b573dd9d65c0b1fb3865f93d0e2abdaa3
Mosaad <48773133+theMosaad@users.noreply.github.com>
2023-05-08 20:20:08 +0300

    Revert "remove serverBuildPath"
    This reverts commit ad7ef6ba49cfe682def8b19a0adf76e9fec431a8.

--- remix.config.js
+++ remix.config.js
@@ -4,6 +4,7 @@ module.exports = {
   // When running locally in development mode, we use the built-in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
+  serverBuildPath: "api/index.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
